daba2e85820fd97f7ee3c496c785a3ec6c1c591a
VVIERV00 <vvierv00@estudiantes.unileon.es>
2016-02-28 22:02:36 +0100

    primer upload del programa

---
+++ simpco.py
@@ -0,0 +1,17 @@
+#Welcome ladies and gentlemen, This is Simpco, my first program deployed on Github, I hope I enjoy it xD
+
+import os
+
+
+Destination_folder = "~/" + input("Dime tu carpeta de Música\n":)
+
+try:
+	if os.path.exists(Destination_folder):
+		pass
+	else os.mkdir(Destination_folder,777):
+
+
+
+
+
+
